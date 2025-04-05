a8c2aa68713a70de3c06b3bc8290c97d3f82359e
grinnch <srirachaexpress@gmail.com>
2025-04-05 00:18:54 -0400

    Update version number

--- palettechanger/__init__.py
+++ palettechanger/__init__.py
@@ -1,3 +1,13 @@
-from .palettechanger import PaletteChanger
+"""
+PaletteChanger: A simple tool to change an image's color palette.
+"""
 
-__version__ = "1.1.0"
+from .palettechanger import PaletteChanger, main
+
+__version__ = "1.2.0"
+__version_info__ = (1, 2, 0)
+__author__ = "grinnch"
+__license__ = "MIT"
+__copyright__ = "Copyright 2023-present grinnch"
+__description__ = "A tool for changing an image's color palette"
+__all__ = ['PaletteChanger', 'main']
